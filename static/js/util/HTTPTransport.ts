8d2f4c3f9c84498c740d08d957db865afbc3b1fa
Anton Voitenko <voytenko.ad@yandex.ru>
2020-07-11 22:05:27 +0800

    Added HTTPTransport

--- static/js/util/HTTPTransport.ts
+++ static/js/util/HTTPTransport.ts
@@ -1,98 +1,116 @@
-interface Window {
-  HTTPTransport: object;
-}
-
-interface IHeaders {
-  [key: string]: string;
-}
-
-type keys = keyof IHeaders;
+type headersStringKey = { [key: string]: string };
+type deepObject = { [key: string]: string | deepObject };
+type modeType = "same-origin" | "no-cors" | "cors";
+type credentialsType = "omit" | "same-origin" | "include";
+type cacheType =
+  | "default»"
+  | "no-store"
+  | "reload"
+  | "no-cache"
+  | "force-cache"
+  | "only-if-cached";
 
 interface IOptions {
-  headers?: IHeaders;
+  headers?: headersStringKey;
   method?: string;
   body?: FormData;
-  mode?: string;
-  credentials?: string;
-  cache?: string;
+  mode?: modeType;
+  credentials?: credentialsType;
+  cache?: cacheType;
   timeout?: number;
 }
 
-// window.HTTPTransport = class {
-//   METHODS: { [key: string]: string } = {
-//     GET: "GET",
-//   };
-//   ERROR_NEED_METHOD = "Need method";
-//   constructor() {
-//     this.queryStringify = this.queryStringify.bind(this);
-//   }
-//   private getDeepParams = (
-//     keyName: string,
-//     object: { [key: string]: any }
-//   ): string => {
-//     return Object.keys(object).reduce((result, key, index, arr) => {
-//       let params: string = `${keyName}[${key}]=${object[key]}`;
+interface IHTTPTransport {
+  METHODS: { GET: string };
+  ERROR_NEED_METHOD: string;
+  getDeepParams(keyName: string, object: deepObject): string;
+  queryStringify(data: deepObject): string;
+  get(url: string, options?: IOptions): Promise<unknown>;
+  request(url: string, options?: IOptions): Promise<unknown>;
+}
 
-//       if (typeof object[key] === "object") {
-//         params = this.getDeepParams(`${keyName}[${key}]`, object[key]);
-//       }
-//       return `${result}${params}${index < arr.length - 1 ? "&" : ""}`;
-//     }, "");
-//   };
+interface Window {
+  HTTPTransport: new () => IHTTPTransport;
+}
 
-//   private queryStringify = (data: { [key: string]: any }) => {
-//     if (typeof data !== "object") {
-//       throw new Error("Data not object");
-//     }
+(function () {
+  class HTTPTransport implements IHTTPTransport {
+    readonly METHODS = {
+      GET: "GET",
+    };
+    readonly ERROR_NEED_METHOD = "Need to specify a method";
+    constructor() {
+      this.queryStringify = this.queryStringify.bind(this);
+    }
+    getDeepParams = (keyName: string, object: deepObject): string => {
+      return Object.keys(object).reduce((result, key, index, arr) => {
+        let obj = object[key];
+        let params: string = `${keyName}[${key}]=${obj}`;
 
-//     const keys = Object.keys(data);
-//     return keys.reduce((result, key, index) => {
-//       let param = `${key}=${data[key]}`;
+        if (typeof obj === "object") {
+          params = this.getDeepParams(`${keyName}[${key}]`, obj);
+        }
+        return `${result}${params}${index < arr.length - 1 ? "&" : ""}`;
+      }, "");
+    };
 
-//       if (typeof data[key] === "object") {
-//         console.log(true);
-//         param = this.getDeepParams(key, data[key]);
-//       }
-//       return `${result}${param}${index < keys.length - 1 ? "&" : ""}`;
-//     }, "");
-//   };
+    queryStringify = (data: deepObject): string => {
+      if (typeof data !== "object") {
+        throw new Error("Data not object");
+      }
 
-//   public get = (url: string, options: IOptions = {}): Promise<unknown> => {
-//     return this.request(url, { ...options, method: this.METHODS.GET });
-//   };
+      const keys = Object.keys(data);
+      return keys.reduce((result, key, index) => {
+        let obj = data[key];
+        let param = `${key}=${obj}`;
 
-//   public request = (url: string, options: IOptions = {}): Promise<unknown> => {
-//     const { headers = {}, method, body, timeout = 5000 } = options;
+        if (typeof obj === "object") {
+          console.log(true);
+          param = this.getDeepParams(key, obj);
+        }
+        return `${result}${param}${index < keys.length - 1 ? "&" : ""}`;
+      }, "");
+    };
 
-//     return new Promise(function (resolve, reject) {
-//       if (!method) {
-//         reject(this.ERROR_NEED_METHOD);
-//         return;
-//       }
+    get = (url: string, options: IOptions = {}): Promise<unknown> => {
+      return this.request(url, { ...options, method: this.METHODS.GET });
+    };
 
-//       const xhr = new XMLHttpRequest();
-//       const isGet = method === this.METHODS.GET;
+    request = (url: string, options: IOptions = {}): Promise<unknown> => {
+      const { headers = {}, method, body, timeout = 5000 } = options;
 
-//       xhr.open(
-//         method,
-//         isGet && !!body ? `${url}${this.queryStringify(body)}` : url
-//       );
+      return new Promise(function (resolve, reject) {
+        if (!method) {
+          reject(this.ERROR_NEED_METHOD);
+          return;
+        }
 
-//       Object.keys(headers).forEach((key) => {
-//         xhr.setRequestHeader(key, headers[key]);
-//       });
+        const xhr = new XMLHttpRequest();
+        const isGet = method === this.METHODS.GET;
 
-//       xhr.onload = () => resolve(xhr);
-//       xhr.onabort = reject;
-//       xhr.onerror = reject;
-//       xhr.timeout = timeout;
-//       xhr.ontimeout = reject;
+        xhr.open(
+          method,
+          isGet && !!body ? `${url}${this.queryStringify(body)}` : url
+        );
 
-//       if (isGet || !body) {
-//         xhr.send();
-//       } else {
-//         xhr.send(body);
-//       }
-//     });
-//   };
-// };
+        Object.keys(headers).forEach((key) => {
+          xhr.setRequestHeader(key, headers[key]);
+        });
+
+        xhr.onload = () => resolve(xhr);
+        xhr.onabort = reject;
+        xhr.onerror = reject;
+        xhr.timeout = timeout;
+        xhr.ontimeout = reject;
+
+        if (isGet || !body) {
+          xhr.send();
+        } else {
+          xhr.send(body);
+        }
+      });
+    };
+  }
+
+  window.HTTPTransport = HTTPTransport;
+})();
